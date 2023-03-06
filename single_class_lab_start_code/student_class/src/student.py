bcf9025c550ab7b7d4fc586a8202f7f46f1d62e9
Paul Cumming <user@users-MacBook-Pro.local>
2023-03-06 15:33:11 +0000

    class creation

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -1,1 +1,10 @@
+class Student: 
+    def __init__(self, name, cohort):
+        self.name = name
+        self.cohort = cohort
+    def talk(self):
+        self.print("I can talk")
+    def say_favourite_language(self, fav_language):
+        self.print(f"I love {fav_language}")
+
 
